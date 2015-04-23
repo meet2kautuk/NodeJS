1606ed1c2d1a9f4dbc9bf6713f4ffae403ea6df7
Kautuk Waghmare <meet2kautuk@gmail.com>
2015-04-23 11:14:41 +0000

    added requires js :-)

--- RateMyPics/index.js
+++ RateMyPics/index.js
@@ -1,18 +1,27 @@
 var express = require("express");
-var appBoostrap = require("./appBoostrap.js");
+var fs = require("fs");
 
 var app = express();
-var appBooter = new appBoostrap();
+
 app.use(express.static("public"));
 
+var appBoostrap = function(bootStrapCompleted){
+    fs.readdir("public/assets/pics", function(err, files){
+        console.log("reading pics...");
+        if(!err){
+            console.log("writing pics.json...");
+            fs.writeFile("public/assets/json/pics.json", JSON.stringify(files), function(errWrite){
+                bootStrapCompleted(errWrite);
+            });
+        }
+    });
+};
 
-appBooter.generatePicsJSON(function(err){
+appBoostrap(function(err){
     if(!err){
         app.listen(process.env.PORT, process.env.IP);    
+        console.log("application is started on:" + process.env.IP + ":" + process.env.PORT);
     }else{
         console.log("error in appBoostrap", err);
     }
 });
-
-
-console.log("application is started on:" + process.env.IP + ":" + process.env.PORT);
---
+++ RateMyPics/public/scripts/app.js
@@ -0,0 +1,11 @@
+require.config({
+    baseUrl: 'scripts',
+    paths: {
+        jquery: 'libs/jquery-1.11.2.min'
+    }
+});
+
+/* actual start point */
+require(["jquery"], function($){
+    
+});
